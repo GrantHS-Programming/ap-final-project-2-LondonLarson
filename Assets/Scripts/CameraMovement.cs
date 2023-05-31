880f1ed54fe8d8ddf5a029ae9956e2dd95ce0411
London <llarson6596@student.pps.net>
2023-05-31 08:31:58 -0700

    azetjmnedz

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject camera;
-    void Start()
-    {
-        //target = pl
-    }
-
     void LateUpdate()
     {
         transform.rotation = Quaternion.identity;
